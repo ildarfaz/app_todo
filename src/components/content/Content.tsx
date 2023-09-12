94a286fd8c19b23b4b57f2c3af0b2a210f5f64bb
ildarfaz <ildarfazylbekov@gmail.com>
2023-09-12 19:43:46 +0500

    del useRef

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,4 +1,4 @@
-import { FC, useReducer, useRef } from "react";
+import { FC, useReducer } from "react";
 import styles from "./content.module.scss";
 import { Input } from "../ui/Input";
 import { Footer } from "../footer/Footer";
