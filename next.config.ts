e77ee2e6c3c1c4996ed25825e95d6803ff2fba6b
ildarfaz <ildarfazylbekov@gmail.com>
2024-11-23 00:53:55 +0500

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
-import type { NextConfig } from "next";
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-const nextConfig: NextConfig = {
-  sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
-  }
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  output: "export",
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true,
+  },
 };
-
-export default nextConfig;
